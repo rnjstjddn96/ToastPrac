192ac1daadd28d70bbbaa4e579a640e4171f1552
rnjstjddn96 <rnjstjddn1996@gmail.com>
2020-07-01 15:28:31 +0900

    IOS에서 Toast 알림 구현하기

--- ToastPrac/ViewController.swift
+++ ToastPrac/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import Toast_Swift
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //self.view.makeToast("토스트 메세지 입니다")
+        
+        // toast presented with multiple options and with a completion closure
+        self.view.makeToast("This is a piece of toast", duration: 2.0, position: .bottom, title: "Toast Title", image: UIImage(named: "04.jpg")) { didTap in
+            if didTap {
+                print("completion from tap")
+            } else {
+                print("completion without tap")
+            }
+        }
     }
 
 
